c73c9e21b6e0faea6de5f22e1eb2c1a6ce7f3db0
kritsanapr <kritsana.pr.41@gmail.com>
2024-11-09 20:47:47 +0700

    setup: prisma and devtools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+    }),
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { AppModule } from './app.module';
 import { VersioningType, Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
 
   app.enableVersioning({
     type: VersioningType.URI,
   });
 
+  await app.listen(process.env.PORT);
   Logger.log(
-    `Server running on http://localhost:${process.env.PORT ?? 3000}`,
+    `Server running on http://localhost:${process.env.PORT}`,
     'Bootstrap',
   );
-  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
