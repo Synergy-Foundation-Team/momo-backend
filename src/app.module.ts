ae2a71b28529b7b89bf700cfd8ecf2b9652a5af0
kritsanapr <kritsana.pr.41@gmail.com>
2024-11-21 23:01:37 +0700

    fix : error app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,23 +10,10 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot([
-      {
-        name: 'short',
-        ttl: 1000,
-        limit: 3,
-      },
-      {
-        name: 'medium',
-        ttl: 10000,
-        limit: 20,
-      },
-      {
-        name: 'long',
-        ttl: 60000,
-        limit: 100,
-      },
-    ]),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 30,
+    }),
     AppConfigModule,
     ModulesModule,
     JwtModule.registerAsync({
